1d52e40fb888ac9ee69ad644af83123fe31b845f
sevil hatipoglu <sevilhatipogluu93@gmail.com>
2021-03-26 13:30:02 +0200

    Added home page design

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/portfolio', to: 'pages#portfolio'
+  get '/mistercocktails', to: 'pages#mistercocktails'
+  get '/bookarts', to: 'pages#bookarts'
+  get '/galata', to: 'pages#galata'
 end
