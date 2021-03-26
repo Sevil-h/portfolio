4950d78d890d57aa48d13ab745d67940c79f048a
sevil hatipoglu <sevilhatipogluu93@gmail.com>
2021-03-26 18:04:01 +0200

    start to do about me page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/portfolio', to: 'pages#portfolio'
+  get '/aboutme', to: 'pages#aboutme'
   get '/mistercocktails', to: 'pages#mistercocktails'
   get '/bookarts', to: 'pages#bookarts'
   get '/galata', to: 'pages#galata'
