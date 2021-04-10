de0f453d01af4b5023b8384aa8de2ad864d7d056
sevil hatipoglu <sevilhatipogluu93@gmail.com>
2021-04-10 18:03:30 +0300

    Navbar color changing when scrolling down

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,10 +20,15 @@ ActiveStorage.start()
 
 // External imports
 import "bootstrap";
+import { initUpdateNavbarOnScroll } from '../components/navbar';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  initUpdateNavbarOnScroll();
+});
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
